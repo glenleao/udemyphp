9b0995d36139e8d0515c6eb5d1fd1226622c520f
Glen Leao <glen@milanezmilaneze.local>
2019-04-26 16:36:57 -0300

    push github

--- include/exemplo2.php
+++ include/exemplo2.php
@@ -1,6 +1,8 @@
 <?php
+//require exige que o arquivo exista e que ele esteja correto; include tenta funcionar mesmo que o arquivo não exista ou que tenha erro. Para evitar o laço (chamar um arquivo repetido) usa-se o _once
 
-include "inc/exemplo1.php";
+require_once "inc/exemplo1.php";
+require_once "inc/exemplo1.php";
 
 $resultado = somar(10, 20);
 
