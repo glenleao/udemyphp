4c6c3c744a75b5d9d3a04b200d71f031b307b03c
glen leao <glenleao@gmail.com>
2019-07-22 22:59:49 -0300

    recaptcha

---
+++ seguranca/exemplo4.php
@@ -0,0 +1,6 @@
+<script src="https://www.google.com/recaptcha/api.js" async defer></script>
+<form action="cadastrar.php" method="post">
+<div class="g-recaptcha" data-sitekey="6Le0-a4UAAAAAKQocYWIkOVbI2ws-NVrEuiSdwKZ"></div>
+	<input type="text" name="inputEmail">
+	<button type="submit">Enviar</button>
+</form>
